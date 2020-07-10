9dbd3ab2820be956108d3eac2c6cbec6f9c44b07
Rafael Guzmán Barranco <iam@guzbarraf.com>
2020-07-10 13:35:54 -0500

    update de gitignore

---
+++ build/precache-manifest.9341baefe4fbdec160205030aa980d72.js
@@ -0,0 +1,222 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "185f7781f40fbedfa328123c1fd95b53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32c4b6bcc242facc245e",
+    "url": "/static/css/main.10bb468b.chunk.css"
+  },
+  {
+    "revision": "ba31119d85dcd1b01f1c",
+    "url": "/static/js/2.a8e3a59a.chunk.js"
+  },
+  {
+    "revision": "c91ede9a3e78add1ac3b6f24a5f90108",
+    "url": "/static/js/2.a8e3a59a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f11d698453548fc390c",
+    "url": "/static/js/3.81c63421.chunk.js"
+  },
+  {
+    "revision": "3262b81f46dd25e912f8",
+    "url": "/static/js/4.2627104c.chunk.js"
+  },
+  {
+    "revision": "b4a7dd2528fcdf7ee7fa",
+    "url": "/static/js/5.2c78684d.chunk.js"
+  },
+  {
+    "revision": "32c4b6bcc242facc245e",
+    "url": "/static/js/main.5a659d23.chunk.js"
+  },
+  {
+    "revision": "13715f6a17530ee8a32d",
+    "url": "/static/js/runtime-main.6ad462c6.js"
+  },
+  {
+    "revision": "1328b74b2e02667ba1328a9c48ba9cca",
+    "url": "/static/media/FTYIRONHORSENCV.1328b74b.ttf"
+  },
+  {
+    "revision": "415790922c51b08d179051682ea43df8",
+    "url": "/static/media/FTYIRONHORSENCV.41579092.woff"
+  },
+  {
+    "revision": "5f7e61167644363380ab8d4253afbce4",
+    "url": "/static/media/FTYIRONHORSENCV.5f7e6116.woff2"
+  },
+  {
+    "revision": "6801af61d860ab65731e918acbd2faa7",
+    "url": "/static/media/FTYIRONHORSENCV.6801af61.eot"
+  },
+  {
+    "revision": "8e3d65668c6d47c8cdf5bbba84e594d9",
+    "url": "/static/media/FTYIRONHORSENCV.8e3d6566.svg"
+  },
+  {
+    "revision": "05dfb07bc08f59b243b9dae41b739331",
+    "url": "/static/media/Lato-Black.05dfb07b.ttf"
+  },
+  {
+    "revision": "10a5402d06e6b37e6518b0edbc727eb2",
+    "url": "/static/media/Lato-Black.10a5402d.svg"
+  },
+  {
+    "revision": "c5fe3435e1171a8895f46a78f4e27089",
+    "url": "/static/media/Lato-Black.c5fe3435.woff"
+  },
+  {
+    "revision": "cdf8b890fcf8b69b8d9f7aab9c474321",
+    "url": "/static/media/Lato-Black.cdf8b890.woff2"
+  },
+  {
+    "revision": "d043b5970486d94ea77567f23ce65b0c",
+    "url": "/static/media/Lato-Black.d043b597.eot"
+  },
+  {
+    "revision": "249def540723f44a8e2a1af57a54873e",
+    "url": "/static/media/Lato-Bold.249def54.eot"
+  },
+  {
+    "revision": "3ae5100d243fd9cf2484599441fdb44d",
+    "url": "/static/media/Lato-Bold.3ae5100d.svg"
+  },
+  {
+    "revision": "5b1a679190934e96b6e99a47ea4d6f39",
+    "url": "/static/media/Lato-Bold.5b1a6791.woff2"
+  },
+  {
+    "revision": "b0d886da2f312da1e7eb54152e361373",
+    "url": "/static/media/Lato-Bold.b0d886da.woff"
+  },
+  {
+    "revision": "d76ff38157f87b8431f361fd621a6a50",
+    "url": "/static/media/Lato-Bold.d76ff381.ttf"
+  },
+  {
+    "revision": "32640cf9b0049dacb9174275ad0a0806",
+    "url": "/static/media/Lato-Regular.32640cf9.woff"
+  },
+  {
+    "revision": "5ba06abb4fad9b01019b64aed25cd316",
+    "url": "/static/media/Lato-Regular.5ba06abb.svg"
+  },
+  {
+    "revision": "77fdd37087fd52db037193aeab1fd85c",
+    "url": "/static/media/Lato-Regular.77fdd370.ttf"
+  },
+  {
+    "revision": "c864a89d88a8c0c88f445d91e9470bfa",
+    "url": "/static/media/Lato-Regular.c864a89d.woff2"
+  },
+  {
+    "revision": "eae698da6399f805fcd1c006438e143d",
+    "url": "/static/media/Lato-Regular.eae698da.eot"
+  },
+  {
+    "revision": "c9d1f6a453d673a310bca758afcf73ac",
+    "url": "/static/media/Smoke-Element.c9d1f6a4.png"
+  },
+  {
+    "revision": "4a59ead1ee8805cfe656643e74342fcb",
+    "url": "/static/media/ahypihey.4a59ead1.png"
+  },
+  {
+    "revision": "b341a5465f3682f782a5d94cf4586f36",
+    "url": "/static/media/ancestral.b341a546.png"
+  },
+  {
+    "revision": "842a0dba979503dec7c9a4aecb9cbf45",
+    "url": "/static/media/beer-company.842a0dba.png"
+  },
+  {
+    "revision": "0bd79ca01eeec540999dfad2691d376f",
+    "url": "/static/media/berlina.0bd79ca0.png"
+  },
+  {
+    "revision": "39781cfac235eb7d7dba911528a41c09",
+    "url": "/static/media/bola-de-cristal.39781cfa.png"
+  },
+  {
+    "revision": "3db9c4339af31fb77be8b65e163450f0",
+    "url": "/static/media/bottle.3db9c433.svg"
+  },
+  {
+    "revision": "b77837054ebcb24e82a54aed06e4131a",
+    "url": "/static/media/esfera.b7783705.png"
+  },
+  {
+    "revision": "4fab3c73146d819e9da004d5028d7f95",
+    "url": "/static/media/hamburguesa.4fab3c73.jpg"
+  },
+  {
+    "revision": "7a5fed7c8dffc60a561a846c709ca07e",
+    "url": "/static/media/hops.7a5fed7c.png"
+  },
+  {
+    "revision": "2c7fb28b78ff1602b016cd13852094ec",
+    "url": "/static/media/huna-hop.2c7fb28b.png"
+  },
+  {
+    "revision": "3c9d934eed93d6382a431d589687b854",
+    "url": "/static/media/logo.3c9d934e.svg"
+  },
+  {
+    "revision": "48ba889e5795ddf4e5ff3507142eae1b",
+    "url": "/static/media/luna.48ba889e.png"
+  },
+  {
+    "revision": "f33179f7c80c893b5d8cd952c9d37743",
+    "url": "/static/media/maestro-diablero.f33179f7.png"
+  },
+  {
+    "revision": "59978df480e389760221742db71b4782",
+    "url": "/static/media/malta.59978df4.png"
+  },
+  {
+    "revision": "148e3588d2109426d93f383857752ab1",
+    "url": "/static/media/mezcalerita.148e3588.png"
+  },
+  {
+    "revision": "019a6d77faa283e6fc8218431c437163",
+    "url": "/static/media/mezcalina.019a6d77.png"
+  },
+  {
+    "revision": "370fbe995417db7305919038c20d4400",
+    "url": "/static/media/papas_adobadas.370fbe99.jpg"
+  },
+  {
+    "revision": "1f541b665ff91ec9fccdf6cb0017a8d3",
+    "url": "/static/media/pinta.1f541b66.svg"
+  },
+  {
+    "revision": "06450cc3c8ed5d475668281b465b3aa5",
+    "url": "/static/media/pizza.06450cc3.jpg"
+  },
+  {
+    "revision": "fc32f8430007173ac0e9bec73bb9be23",
+    "url": "/static/media/quickText.fc32f843.png"
+  },
+  {
+    "revision": "dc081fa14d2e1fe9e2e7666c9699a25b",
+    "url": "/static/media/smoke.dc081fa1.png"
+  },
+  {
+    "revision": "fa2cd97705c2415a4a85d35626073d5f",
+    "url": "/static/media/snifter.fa2cd977.svg"
+  },
+  {
+    "revision": "59de8240ddb00e4eeba630e58d61073d",
+    "url": "/static/media/tendajon.59de8240.png"
+  },
+  {
+    "revision": "f71aef933e0ff165a522580a13d592c1",
+    "url": "/static/media/txalaparta.f71aef93.png"
+  },
+  {
+    "revision": "4261a6f3cb94d1477ef2a7e15ec12c24",
+    "url": "/static/media/zandunga.4261a6f3.png"
+  }
+]);
